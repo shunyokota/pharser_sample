e93ca1c4a3c09c4ba9851a4c70af8213ce3865e1
shunyokota <shunyokota@gmail.com>
2022-04-03 22:53:07 +0900

    とりあえずプレイヤーを動かしてみる

---
+++ src/consts/CofigConstants.ts
@@ -0,0 +1,4 @@
+export class CofigConstants {
+    public static displayWidth: number = 800;
+    public static displayHeight: number = 400;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,90 @@
 import Phaser from 'phaser';
 import logoImg from '~/assets/logo.png';
+import prayerImg from '~/assets/prayer.png';
+import {CofigConstants} from "~/consts/CofigConstants";
 
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
+class MyGame extends Phaser.Scene {
+    // private sprite: Phaser.GameObjects.Sprite;
+    private prayer: Phaser.GameObjects.Image;
+    private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+
+    constructor() {
         super('sample-scene');
     }
 
-    preload ()
-    {
+    preload() {
         this.load.image('logo', logoImg);
+        this.load.image('prayer', prayerImg);
+
     }
 
-    create ()
-    {
-        const logo = this.add.image(400, 150, 'logo');
+    create() {
+        // this.sprite = this.add.sprite(200, 300, 'prayer');
+        // this.sprite.setScale(0.5);
+        this.prayer = this.add.image(400, 150, 'prayer').setScale(1).setAngle(-40);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.add.rectangle(3 * CofigConstants.displayWidth / 4 - 20, 20, CofigConstants.displayWidth / 2, 20, 0xff0000);
+        // sprite.setRotation(10);
+        // sprite.
+        // sprite.anchor.setTo(0.5, 0.5);
+        //
+        // const logo = this.add.image(400, 150, 'logo');
+        //
+        // this.tweens.add({
+        //     targets: logo,
+        //     y: 450,
+        //     duration: 2000,
+        //     ease: "Power2",
+        //     yoyo: true,
+        //     loop: -1
+        // });
+
 
         this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
+            targets: this.prayer,
+            angle: 40,
+            duration: 700,
             yoyo: true,
             loop: -1
         });
     }
+
+    update() {
+        if (this.cursors.left.isDown)
+        {
+            this.prayer.setX(this.prayer.x - 1);
+        }
+        if (this.cursors.right.isDown)
+        {
+            this.prayer.setX(this.prayer.x + 1);
+        }
+        if (this.cursors.up.isDown)
+        {
+            this.prayer.setY(this.prayer.y - 1);
+        }
+        if (this.cursors.down.isDown)
+        {
+            this.prayer.setY(this.prayer.y + 1);
+        }
+        // this.sprite.angle += 1;
+
+        // text.setText([
+        //     'Sprite Rotation',
+        //     'Angle: ' + arrow.angle.toFixed(2),
+        //     'Rotation: ' + arrow.rotation.toFixed(2)
+        // ]);
+    }
+
 }
 
 const config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
-    width: 800,
-    height: 600,
+    width: CofigConstants.displayWidth,
+    height: CofigConstants.displayHeight,
+    backgroundColor: '#4488aa',
     scene: MyGame
 };
 
